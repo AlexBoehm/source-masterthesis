e8ec82dfeff331027f85f6f9977eb349833856d6
Alexander Böhm <alex@alebo.de>
2021-06-25 15:27:37 +0200

    Verschiedene Beispiele der Features von Blazor

---
+++ Masterarbeit/Pages/Components/PartialClasses/Page.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace Masterarbeit.Pages.Components.PartialClasses {
+    public partial class Page {
+        private int currentCount = 0;
+
+        private void IncrementCount() {
+            currentCount++;
+        }
+    }
+}
